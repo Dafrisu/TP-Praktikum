f2c7c99a9872e5f3d0739c7dc73bdb144906c26a
Dafrisu <daffaraymisuandi@gmail.com>
2024-03-03 18:11:31 +0700

    tambahan fitur GUI

---
+++ TP_MOD_03_1302223156_DAFARAIMISUANDI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace TP_MOD_03_1302223156_DAFARAIMISUANDI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult hasil = MessageBox.Show(textBox1.Text);
+            label1.Text = "Halo " + textBox1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
